483145aa4f66ccf0fcafff3bb695f0e473d78b23
Marc Concepcion <marcanthonyconcepcion@gmail.com>
2021-01-29 17:26:59 +0800

    Add time stamp on log file.

--- RunProcess.php
+++ RunProcess.php
@@ -2,10 +2,11 @@
 
 function run_process()
 {
+    $log_file_name = "time_now_".microtime().".log";
     while(TRUE)
     {
         $date_time = (new Datetime("now", new DateTimeZone('Asia/Manila')))->format('Y-m-d H:i:s');
-        $log_file = fopen("time_now.log", "a");
+        $log_file = fopen($log_file_name, "a");
         fwrite($log_file, "Right now, it is $date_time!".PHP_EOL);
         fclose($log_file);
         sleep(60);
